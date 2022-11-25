24f215b2449ca3921768ea14907115889c4dd251
genesis-g101 <gyreplicant@gmail.com>
2022-11-25 18:42:40 -0300

    colocando keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,8 +6,10 @@ export const Expenses = props => {
   return (
     <Card className="expenses">
       {props.expenses.map(expense => (
-        <ExpenseItem expense={expense} />
+        // sempre deve ter um index quando interar sobre listas
+        <ExpenseItem key={expense.id} expense={expense} />
       ))}
+      {/* isso evita a repetição abaixo */}
       {/* <ExpenseItem expense={props.expenses[0]} />
       <ExpenseItem expense={props.expenses[1]} />
       <ExpenseItem expense={props.expenses[2]} />
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,7 @@ export const ExpenseForm = props => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: enteredDate,
+      date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
