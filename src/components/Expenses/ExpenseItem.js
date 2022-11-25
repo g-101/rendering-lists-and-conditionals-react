2eae0f238badb9fcae101a80c175815079cc2b6d
genesis-g101 <gyreplicant@gmail.com>
2022-11-25 11:27:30 -0300

    fix: title em ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
-import './ExpenseItem.css';
-import React, { useState } from 'react';
 import { Card } from '../UI/Card';
 import { ExpenseDate } from './ExpenseDate';
+import './ExpenseItem.css';
 
 export const ExpenseItem = props => {
-  const [title, setTitle] = useState(props.expense.title);
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.expense.title}</h2>
         <div className="expense-item__price">${props.expense.amount}</div>
       </div>
-
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
