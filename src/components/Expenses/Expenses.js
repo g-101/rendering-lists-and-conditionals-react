ad55c6177d4bdca2d492eadea93413c8522495e1
genesis-g101 <gyreplicant@gmail.com>
2022-11-25 20:13:26 -0300

    filtrando pelo ano

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import './Expenses.css';
 import { Card } from '../UI/Card';
 import { ExpenseItem } from './ExpenseItem';
+import { ExpensesFilter } from './ExpensesFilter';
 
 export const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.expenses.filter(
+    expense => expense.date.getFullYear().toString() === filteredYear
+  );
   return (
     <Card className="expenses">
-      {props.expenses.map(expense => (
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {filteredExpenses.map(expense => (
         // sempre deve ter um index quando interar sobre listas
         <ExpenseItem key={expense.id} expense={expense} />
       ))}
